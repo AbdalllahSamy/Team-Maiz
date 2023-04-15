8539e8c255011323d930ff1bd24345352a9ffffa
Abdallah Samy <128118319+AbdalllahSamy@users.noreply.github.com>
2023-04-15 06:42:05 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,19 @@
 let nav_scroll = document.querySelector(".fixed-nav");
 let list_scroll = document.querySelector(".nav-list__link");
 let button_scroll = document.querySelector(".nav-button");
+let view_more = document.getElementById('more');
+let view_more_players = document.getElementById('another-players');
+let show_less = document.getElementById('less');
+
+view_more.onclick = function() {
+    view_more_players.style.display = 'block';
+    view_more.style.display = 'none';
+};
+
+show_less.onclick = function() {
+    view_more_players.style.display = 'none';
+    view_more.style.display = 'inline';
+};
 
 window.onscroll = function() {
     if (this.scrollY >= 100) {
